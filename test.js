b089256d295720a777fae1fd1d6d4227b8817531
Jamie <Jamiehyland01@gmail.com>
2020-04-03 14:49:24 +0100

    Test methods for file chunking

---
+++ test.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+//Retrieve the text file as one big string
+var str = fs.readFileSync('data.txt', 'utf8');
+//Convert into an array of strings, each string in the format number character character number
+var data = str.split("\n");
+
+//Get chunk from file, with letter count and word count
+
+
+//write results to file
+
+
+//

05e424022a917ea06994c612611bf043448eadea
Shayne Monahan <shayne.monahan4@mail.dcu.ie>
2020-04-12 15:56:18 +0100

    Adding socket.io and form handlers

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
 const socket = require("socket.io");
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
+var fs = require('fs');
 
+var routes = require('./routes/index');
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.json());
 app.set('view engine', 'pug');
 
-app.get('/', function (req, res) {
-  res.render('index');
-});
-   
+app.use('/', routes);
 
 var server = app.listen(3000, () => {
   console.log(`server is running on port`, server.address().port);
 });
 
+var io = socket(server);
+
+io.sockets.on('connection', function(socket) {
+  console.log("New Client: " + socket.id);
+
+  socket.on('new_chunks', function(msg){
+    console.log(socket.id + ": " + msg);
+  });
+
+  socket.on('new_file', function(file_str){
+    console.log("File Output: \n" + file_str);
+  });
+
+  socket.on('disconnect', function() {
+    console.log("Client has Disconnected");
+  });
+}); 
+
+module.exports = app;
    
--- public/javascript/chunks.js
+++ public/javascript/chunks.js
@@ -1,3 +1,10 @@
+var socket;
+
+socket = io.connect('http://localhost:3000');
+
+
+
+
 $(document).ready(function() {
     $(window).keydown(function(event){
         // prevent "enter" key from sending form
@@ -6,8 +13,40 @@ $(document).ready(function() {
         return false;
         }
     });
+
+    $('#file_upload').submit(function(e) {
+        $('#upload-form').css("display","none");
+        var gtxtFile = $('#txt-file').prop('files');
+
+        const reader = new FileReader();
+        reader.onload = function () {
+            var txtFile = reader.result;
+            socket.emit('new_file', txtFile);
+        }
+        reader.readAsText(gtxtFile[0]);
+
+        e.preventDefault();
+    });
+
+
+    $('#new-chunks').submit(function(e) {
+        $('#chunks-form').css("display","none");
+        var chunkNum = $('#num-chunks').val();
+        var chunks = [];
+        chunks.push(chunkNum);
+        for (var i = 1; i <= chunkNum; i++) {
+            var chunk = [];
+            chunk.push($('#chunk_start_'+i).val());
+            chunk.push($('#chunk_end_'+i).val());
+            chunks.push(chunk);
+        }
+        socket.emit('new_chunks', chunks);
+        e.preventDefault();
+    });
 });
 
+
+// make sure file uploaded is a txt file
 function checkExt() {
     if(document.file_upload.text_file.value.lastIndexOf(".txt") == -1) {
         $('#txt-file').val('');
@@ -15,6 +54,7 @@ function checkExt() {
         return false;
     }
 }
+
 
 // open/close New Text File form pop up
 $('#upload').click(function() {
@@ -36,34 +76,16 @@ $('.exit').click(function() {
 	$('#chunks-form').css("display","none");
 });
 
+
+// add as many inputs as chunks selected
 function chunkSel() {
     var val = $('#num-chunks').val();
     $('#chunks-wrap').empty();
     // add input for chunks
-    var j = 0;
     for (var i = 1; i <= val; i++) {
-        if (i == 1) {
             $('<label class="d-block control-label">Chunk ' + i + '</label>').appendTo($('#chunks-wrap'));
             $('<div id= "side-' + i + '" class="input-group mb-2"></div>').appendTo($('#chunks-wrap'));
-            // A default for 1st chunk
-            $('<input type="text" class="form-control text-uppercase" placeholder="Start" value="A" disabled/>').appendTo($('#side-'+i));
-            $('<input type="text"  class="form-control text-uppercase" placeholder="End" pattern="[A-Za-z]{1}" title="Only 1 Letter" required/>').appendTo($('#side-'+i));
-            j++;
-        }
-        else if (i == val) {
-            $('<label class="d-block control-label">Chunk ' + i + '</label>').appendTo($('#chunks-wrap'));
-            $('<div id= "side-' + i + '" class="input-group mb-5"></div>').appendTo($('#chunks-wrap'));
-            $('<input type="text" class="form-control text-uppercase" placeholder="Start" pattern="[A-Za-z]{1}" title="Only 1 Letter" required/>').appendTo($('#side-'+i));
-            // Z default for last chunk
-            $('<input type="text" class="form-control text-uppercase" placeholder="End" value="Z" disabled/>').appendTo($('#side-'+i));
-        }
-        else {
-            $('<label class="d-block control-label">Chunk ' + i + '</label>').appendTo($('#chunks-wrap'));
-            $('<div id= "side-' + i + '" class="input-group mb-2"></div>').appendTo($('#chunks-wrap'));
-            $('<input type="text" class="form-control text-uppercase" placeholder="Start"  pattern="[A-Za-z]{1}" title="Only 1 Letter" required/>').appendTo($('#side-'+i));
-            j++;
-            $('<input type="text" class="form-control text-uppercase" placeholder="End"  pattern="[A-Za-z]{1}" title="Only 1 Letter" required/>').appendTo($('#side-'+i));
-            j++;
-        }
+            $('<input id="chunk_start_'+i+'" type="text" class="form-control text-uppercase" placeholder="Start"  pattern="[A-Za-z]{1}" title="Only 1 Letter" required/>').appendTo($('#side-'+i));
+            $('<input id="chunk_end_'+i+'" type="text" class="form-control text-uppercase" placeholder="End"  pattern="[A-Za-z]{1}" title="Only 1 Letter" required/>').appendTo($('#side-'+i));
     }
 }
